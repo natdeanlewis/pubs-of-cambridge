c72da346e9b827aacb8349302194b7f280036211
natdeanlewissoftwire <nat.dean-lewis@softwire.com>
2024-11-03 23:17:33 +0000

    feat: random unvisited pub recommendation

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -10,23 +10,9 @@ export default function Map() {
     const INITIAL_BEARING = 0;
     const [pubs, setPubs] = useState([]);
     const [visitedPubs, setVisitedPubs] = useState([]);
+    const [randomPub, setRandomPub] = useState([]);
     const markers = useRef([]);
     const lotrMapStyle = 'mapbox://styles/natdeanlewis/cm31fd4i300vc01pigpm06fr3/draft';
-
-    useEffect(() => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoibmF0ZGVhbmxld2lzIiwiYSI6ImNtMzBjcWpkNjBpaXgybXNhdGYyYTU2Y3AifQ.lM4WFOgR19cbYIGR5seCCg';
-        
-        mapRef.current = new mapboxgl.Map({
-            container: mapContainerRef.current,
-            style: lotrMapStyle,
-            center: INITIAL_CENTER,
-            zoom: INITIAL_ZOOM,
-            pitch: INITIAL_PITCH,
-            bearing: INITIAL_BEARING,
-        });
-
-        return () => mapRef.current.remove();
-    }, []);
 
     useEffect(() => {
         async function getPubs() {
@@ -51,6 +37,20 @@ export default function Map() {
 
         getPubs();
         getVisitedPubs();
+        setRandomPub(null);
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoibmF0ZGVhbmxld2lzIiwiYSI6ImNtMzBjcWpkNjBpaXgybXNhdGYyYTU2Y3AifQ.lM4WFOgR19cbYIGR5seCCg';
+        
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: lotrMapStyle,
+            center: INITIAL_CENTER,
+            zoom: INITIAL_ZOOM,
+            pitch: INITIAL_PITCH,
+            bearing: INITIAL_BEARING,
+        });
+
+        return () => mapRef.current.remove();
     }, []);
 
     useEffect(() => {
@@ -120,7 +120,8 @@ export default function Map() {
         setVisitedPubs(userData.visited_pub_ids);
     }
 
-    const handleButtonClick = () => {
+    const handleResetViewClick = () => {
+        setRandomPub(null)
         mapRef.current.flyTo({
             center: INITIAL_CENTER,
             zoom: INITIAL_ZOOM,
@@ -129,20 +130,57 @@ export default function Map() {
         });
     };
 
+    const handleRandomPubClick = () => {
+        const unvisitedPubs = pubs.filter((pub) => !visitedPubs.includes(pub._id)).filter((pub) => !visitedPubs.includes(pub._id))
+        const randomChoice = Math.floor(Math.random() * unvisitedPubs.length)
+        const randomPub = unvisitedPubs[randomChoice]
+        
+        setRandomPub(randomPub)
+
+        mapRef.current.flyTo({
+            center: [randomPub.longitude, randomPub.latitude],
+            zoom: 16,
+            pitch: INITIAL_PITCH,
+            bearing: INITIAL_BEARING,
+        });
+    };
+      
     return (
         <div className="relative h-screen">
-            <button
-                className='absolute top-4 left-4 bg-yellow-700 hover:bg-yellow-900 text-white font-bold py-2 px-4 rounded z-10'
-                onClick={handleButtonClick}
-            >
-                Reset view
-            </button>
-            {pubs.length > 0 &&
-                <p className='absolute top-4 right-4 bg-yellow-700 text-white font-bold py-2 px-4 rounded z-10'>
-                    Visited: {visitedPubs.length}/{pubs.length}
-                    {visitedPubs.length === pubs.length ? ' 🥳' : ''}
-                </p>        
+            <div className="absolute inline-flex top-4 left-4">     
+                <button
+                    className='m-2 bg-yellow-700 hover:bg-yellow-900 text-white font-bold py-2 px-4 rounded z-10'
+                    onClick={handleRandomPubClick}
+                >
+                    Pub?
+                </button>
+
+                <button
+                    className='m-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded z-10'
+                    onClick={handleResetViewClick}
+                >
+                    Reset view
+                </button>
+            </div>
+
+            {!!randomPub && 
+            <div className="absolute w-full flex justify-center top-4">
+                    <div className='m-16 py-2 px-4 z-10 bg-neutral-800 rounded text-white font-bold'>
+                        How about... The {randomPub.name}?
+                    </div>
+            </div>
             }
+
+            <div className="absolute inline-flex top-4 right-4">     
+                {pubs.length > 0 &&
+                    <p className='m-2 text-white font-bold bg-neutral-800 py-2 px-4 rounded z-10'>
+                        Visited: {visitedPubs.length}/{pubs.length}
+                        {visitedPubs.length === pubs.length ? ' 🥳' : ''}
+                    </p>        
+                }
+
+            </div>
+
             <div id='map-container' className='h-full' ref={mapContainerRef} />
         </div>
     );
