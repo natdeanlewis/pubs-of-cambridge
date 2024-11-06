2b25453dbc3bae52c0cd2b4d36864ed5580aa02f
natdeanlewis <nat.deanlewis@gmail.com>
2024-11-06 10:42:05 +0000

    feat: add user marker back in

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -93,13 +93,37 @@ export default function Map() {
                     ...INITIAL_MAP_SETTINGS,
                 });
             };
-    
+
             const initialize = async () => {
                 await Promise.all([fetchPubs(), fetchVisitedPubs(), initMap()]);
             };
-    
+
             initialize();
-    
+
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const userMarker = document.createElement('div');
+                    userMarker.className = 'group hover:z-20';
+                    userMarker.style.zIndex = '10';
+            
+                    userMarker.innerHTML = `
+                        <span class="relative flex items-center justify-center text-2xl">
+                             <span class="animate-ping absolute inline-flex rounded-full">👑</span>
+                            <span class="absolute inline-flex rounded-full" style="text-shadow: 1px 1px 0 black, -1px -1px 0 black, -1px 1px 0 black, 1px -1px 0 black;">👑</span>
+                        </span>
+                    `;
+                    const label = document.createElement('div');
+                    label.className = 'absolute bottom-[-32px] left-1/2 transform -translate-x-1/2 bg-amber-100 px-1 rounded shadow text-xs whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-300 font-serif italic';
+                    label.textContent = `You!`;
+                
+                    userMarker.appendChild(label);
+                    
+                    new mapboxgl.Marker(userMarker)
+                        .setLngLat([position.coords.longitude, position.coords.latitude])
+                        .addTo(mapRef.current);
+                });
+            }
+
             return () => {
                 if (mapRef.current) {
                     mapRef.current.remove();
