f2573ebd86485d2d346be7853f89de7b288ced7b
natdeanlewis <79046379+natdeanlewis@users.noreply.github.com>
2024-11-11 21:24:06 +0000

    feat: brownify

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ label, handleClickAction, style }) {
     let classes = "m-2 py-2 px-4 rounded";
     if (style === "brown") {
         classes +=
-            " bg-yellow-700 active:bg-yellow-900";
+            " bg-yellow-900 active:bg-yellow-950";
     } else {
         classes +=
             " bg-gray-400 active:bg-gray-600";
