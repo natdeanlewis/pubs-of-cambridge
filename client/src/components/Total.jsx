d04406d34944c8497242c0f1a7355c053f881f66
natdeanlewis <79046379+natdeanlewis@users.noreply.github.com>
2024-11-11 21:25:45 +0000

    feat: brownify total

--- client/src/components/Total.jsx
+++ client/src/components/Total.jsx
@@ -2,7 +2,7 @@ export default function Total({ pubs, visitedPubs }) {
     return (
         <div>
             {pubs.length > 0 && (
-                <p className="m-2 text-white font-bold bg-yellow-700 py-2 px-4 rounded font-serif">
+                <p className="m-2 text-white font-bold bg-yellow-900 py-2 px-4 rounded font-serif">
                     <span className="italic">
                         {visitedPubs.length}/{pubs.length}
                     </span>
