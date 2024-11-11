27d877c88a098b943d923a55dd6222b8779ae6a6
natdeanlewis <79046379+natdeanlewis@users.noreply.github.com>
2024-11-11 21:09:12 +0000

    feat: more focus tweaks

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -2,10 +2,10 @@ export default function Button({ label, handleClickAction, style }) {
     let classes = "m-2 py-2 px-4 rounded";
     if (style === "brown") {
         classes +=
-            " bg-yellow-700 hover:bg-yellow-900 focus:bg-yellow-700 active:bg-yellow-900 focus-visible:bg-yellow-700";
+            " bg-yellow-700 hover:bg-yellow-900 focus:bg-yellow-700 active:bg-yellow-900";
     } else {
         classes +=
-            " bg-gray-500 hover:bg-gray-700 focus:bg-gray-500 active:bg-gray-700 focus-visible:bg-gray-500";
+            " bg-gray-500 hover:bg-gray-700 focus:bg-gray-500 active:bg-gray-700";
     }
 
     return (
