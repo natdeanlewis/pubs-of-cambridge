10655b836ae5f7f386c1cf84b2c5ac25f1153f57
natdeanlewis <nat.deanlewis@gmail.com>
2024-11-13 13:43:58 +0000

    feat: update name

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'pubsofcambridge.vercel.app',
-  appName: 'Pubs of Cambridge',
+  appName: 'Pubs',
   webDir: 'dist'
 };
 
