18503419b93a8f2038fdb11dec69fcc39c2c3fec
natdeanlewis <nat.deanlewis@gmail.com>
2024-11-13 17:40:56 +0000

    feat: increase zoom threshold for unvisited

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -322,7 +322,7 @@ export default function Map() {
 
         const updateLabelOpacity = () => {
             const zoom = mapRef.current.getZoom();
-            if (zoom > 15.5 || (zoom > 13.5 && !visitedPubs.includes(pub._id))) {
+            if (zoom > 16 || (zoom > 13.5 && !visitedPubs.includes(pub._id))) {
                 label.style.opacity = "1";
             } else {
                 label.style.opacity = null;
