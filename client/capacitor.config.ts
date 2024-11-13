5df35060d19004521d0e172de05600e30b9b681e
natdeanlewis <nat.deanlewis@gmail.com>
2024-11-13 13:05:25 +0000

    feat: add ios app with capacitor

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pubsofcambridge.vercel.app',
+  appName: 'Pubs of Cambridge',
+  webDir: 'dist'
+};
+
+export default config;
